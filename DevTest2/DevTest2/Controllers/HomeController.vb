8cd535ac691225f9aee69eea3d18efb1cc1f0156
DESKTOP-CJ76F6P\khana <khanal.raj124@gmail.com>
2019-04-08 12:15:03 -0500

    Final Commit with reason for refactored code
    •	Changed the type of SalesTotalInfo from string to StringBuilder.
    •	Moved the count and sales total sum into the same linq query where we get customer details. Getting all the data in one single linq query will reduce the time to get data instead of connecting to database multiple times.
    •	Instead of writing response to web page for each record or each loop, all the data is appended to a SalesTotalInfo object and final string is written to webpage. This saves write time.

---
+++ DevTest2/DevTest2/Controllers/HomeController.vb
@@ -0,0 +1,35 @@
+﻿Imports System.Data.Entity
+
+Public Class HomeController
+    Inherits System.Web.Mvc.Controller
+
+    Function Index() As ActionResult
+
+        Return View()
+    End Function
+
+    Function CsvReport() As ActionResult
+        Dim SalesTotalInfo As StringBuilder = New StringBuilder()
+        'Response.ContentType = "text/plain"
+        'Response.Write("CustomerId, Name, OrderCount, SalesTotal" & vbCr)
+        SalesTotalInfo.Append("CustomerId, Name, OrderCount, SalesTotal" & vbCr)
+        Using db As New AppDbContext()
+
+            'get customers
+            Dim customers = (From c In db.Customers
+                             Order By c.CustomerId
+                             Select New With {c, c.Orders.Count, c.Orders.Select(Function(x) x.SalesTotal).Sum()}).ToList() ', c.Orders.Count()
+
+            For Each cust In customers
+                SalesTotalInfo.Append(String.Format("{0}, {1}, {2}, {3}", cust.c.CustomerId, cust.c.Name, cust.Count, cust.Sum.ToString("0.##") & vbCr))
+            Next
+
+        End Using
+        Response.ContentType = "text/plain"
+        Response.Write(SalesTotalInfo)
+        Response.End()
+
+        Return Nothing
+    End Function
+
+End Class
